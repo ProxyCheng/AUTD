1a9e4eb09748302af740b20651283ffbb7e29e70
CZQ_NB <3224272275@qq.com>
2026-08-02 13:08:37 +0800

    card

--- runtime/frontend/modes/building_mode.gd
+++ runtime/frontend/modes/building_mode.gd
@@ -1,8 +1,11 @@
 class_name BuildingMode
 extends Mode
 
+var building_model: Node3D = null
+
 func _ready():
 	$"ui".hide()
+	$ui/cards/card_crossbow.clicked.connect(func(): _on_card_clicked("crossbow"))
 
 func enter():
 	$"ui".show()
@@ -10,9 +13,14 @@ func enter():
 func tick(in_delta: float):
 	if Input.is_key_pressed(KEY_ESCAPE):
 		owner.set_mode(&"roaming")
-	var axis = get_pointing_axis()
-	var cell = Level.current.map.get_cell(axis)
-	print(cell.land.type if cell and cell.land else "")
+	var axis: Vector2i = get_pointing_axis()
+	var cell: Cell = Level.current.map.get_cell(axis)
+	if building_model:
+		if not cell:
+			building_model.hide()
+			return
+		building_model.position = Vector3(axis.x, 0, axis.y)
+		building_model.show()
 	
 func get_pointing_axis():
 	var viewport: Viewport = get_viewport()
@@ -34,4 +42,20 @@ func ray_intersects_y0(in_origin: Vector3, in_direction: Vector3):
 	return in_origin + in_direction * t
 
 func leave():
+	if building_model:
+		remove_child(building_model)
+		building_model.queue_free()
+		building_model = null
 	$"ui".hide()
+
+func _on_card_clicked(in_building_id: String):
+	if building_model:
+		remove_child(building_model)
+		building_model.queue_free()
+		building_model = null
+	var building_path: String = "res://runtime/frontend/models/buildings/%s/%s.tscn" % [in_building_id, in_building_id]
+	var building_scene: PackedScene = load(building_path)
+	building_model = building_scene.instantiate()
+	add_child(building_model)
+	building_model.owner = owner
+	building_model.hide()
--- runtime/frontend/ui/building_card.gd
+++ runtime/frontend/ui/building_card.gd
@@ -1,9 +1,10 @@
 extends Control
 class_name BuildingCard
 
+signal clicked()
+
 func _ready():
 	%model_renderer.position = Vector3.DOWN * 1000
 
-
 func _on_panel_pressed() -> void:
-	pass # Replace with function body.
+	clicked.emit()
